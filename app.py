d0e8b247dcfeeadb8fc241607cc45ed978617b8c
ShrishPre <shristijain1525@gmail.com>
2026-01-13 00:50:07 +0530

    Update Streamlit app logic

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 st.set_page_config(page_title="Customer Churn Predictor",page_icon="📉", layout="centered")
 
 # Load model
+import pickle
 with open("gb.pkl", "rb") as file:
     data = pickle.load(file)
 
